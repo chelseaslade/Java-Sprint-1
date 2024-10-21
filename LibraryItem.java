bafbe6e70f10baed9cb401aa254e35a2bb98e01c
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-21 14:39:22 -0230

    Library item class/subclasses attributes & constructors

--- LibraryItem.java
+++ LibraryItem.java
@@ -4,13 +4,50 @@ public class LibraryItem {
     protected int numCopies;
 
     //Constructors
+    public LibraryItem(String id, String title, String author, String ISBN, String publisher, int numCopies) 
+    {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.publisher = publisher;
+        this.numCopies = numCopies;
+    }
 
     //Methods
-    //addNewItem()
-    //editItem()
-    //deleteItem()
+}
 
-    //Subclasses
-    //Periodical
-    //Books
+//Subclasses
+//Periodical
+class Periodical extends LibraryItem {
+    //Attributes
+    protected boolean isPrinted, isElectronic;
+
+    //Constructor
+    public Periodical(String id, String title, String author, String ISBN, String publisher, int numCopies, boolean isPrinted, boolean isElectronic) 
+    {
+        super(id, title, author, ISBN, publisher, numCopies);
+        this.isElectronic = isElectronic;
+        this.isPrinted = isPrinted;
+    }
+}
+
+//Books
+class Book extends LibraryItem {
+    //Attributes
+    protected boolean isPrinted, isElectronic, isAudio;
+
+    //Constructor
+    public Book(String id, String title, String author, String ISBN, String publisher, int numCopies, boolean isPrinted, boolean isElectronic, boolean isAudio) 
+    {
+        super(id, title, author, ISBN, publisher, numCopies);
+        this.isElectronic = isElectronic;
+        this.isPrinted = isPrinted;
+        this.isAudio = isAudio;
+    }
+
+//Methods
+//addNewItem()
+//editItem()
+//deleteItem()
 }
